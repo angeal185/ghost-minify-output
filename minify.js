19924ed8af6edea03839d472268c69df9ef9c7b3
Angeal185 <beneaves01@hotmail.com>
2017-06-14 11:48:06 +0800

    Add files via upload

---
+++ minify.js
@@ -0,0 +1,34 @@
+/* to be placed at ghost/core/server/middleware/minify/ */
+var Minifier, exports;
+
+Minifier = require('html-minifier');
+
+module.exports = exports = function(req, res, next) {
+  res.oldRender = res.render;
+  res.render = function(view, options) {
+    this.oldRender(view, options, function(err, html) {
+      if (err) {
+        throw err;
+      }
+      html = Minifier.minify(html, {
+        caseSensitive: false,
+        removeComments: true,
+        removeCommentsFromCDATA: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true,
+        collapseBooleanAttributes: true,
+        removeAttributeQuotes: false,
+        removeEmptyAttributes: true,
+        minifyJS: true,
+        minifyCSS: true,
+        removeScriptTypeAttributes: false,
+        removeStyleLinkTypeAttributes: false,
+        sortClassName: false,
+        sortAttributes: false,
+		processScripts:[ 'application/ld+json' ]
+      });
+      res.send(html);
+    });
+  };
+  next();
+};
